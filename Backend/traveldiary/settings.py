5a08fa7bd3901ae3b309844ae0667eca76156556
zcetin <46650409+zcetin@users.noreply.github.com>
2020-04-01 19:28:38 +0300

    Update settings.py

--- Backend/traveldiary/settings.py
+++ Backend/traveldiary/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'tripsdb',
         'USER': 'root',
-        'PASSWORD': 'zeynepsabri02',
+        'PASSWORD': '********',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
